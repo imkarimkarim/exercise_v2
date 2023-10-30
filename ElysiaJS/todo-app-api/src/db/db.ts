9b71522f76eb360cacdb679e7f38fe9aeedb9218
imkarimkarim <imkarimkarim@pm.me>
2023-10-30 19:43:13 +0330

    Untrack files in .gitignore

--- ElysiaJS/todo-app-api/src/db/db.ts
+++
@@ -1,6 +0,0 @@
-import mongoose from 'mongoose';
-
-const connection = process.env.MONGODB_URI ?? 'mongodb://localhost:27017';
-
-mongoose.connect(connection).then(() => console.log('Connected to DataBase Successfully!'));
-export default mongoose;
--- expo/StickerSmash/AppStyles.ts
+++
@@ -1,9 +0,0 @@
-import { StyleSheet } from 'react-native';
-
-export const appStyles = StyleSheet.create({
-	container: {
-		height: '100%',
-		width: '100%',
-		backgroundColor: '#25292e',
-	},
-});
