3b6389f5b567345e3000200766e83dabe44cd9f6
imkarimkarim <imkarimkarim@pm.me>
2023-10-30 19:36:42 +0330

    feat(ElysiaJS): add handlers

--- ElysiaJS/todo-app-api/src/routes/v1/todos/todos.ts
+++ ElysiaJS/todo-app-api/src/routes/v1/todos/todos.ts
@@ -1,9 +1,59 @@
-import { Elysia } from 'elysia';
+import { Elysia, t } from 'elysia';
 
 export const todos = new Elysia({ prefix: '/todos' })
-	// /todos create(POST(title: string, done: boolean, nice: maxNice(+1), dateAdded: timestamp, dateUpdated: timeStamp))
-	// /todos showAll(GET)
-	// /todos/:id show(GET)
-	// /todos/:id delete(DELETE)
-	// /todos/:id update(PATCH)
-	.get('/', () => 'TODOS WILL BE HERE SOON');
+	// schema
+	.model({
+		todo: t.Object({
+			title: t.String(),
+			description: t.String({ default: '' }),
+			isDone: t.Boolean({ default: false }),
+			nice: t.Number({ default: 0 }), // maxNice(+1)
+		}),
+	})
+
+	// GET ALL
+	.get('/', () => {
+		return 'here will all the todo will be listed';
+	})
+
+	// GET /:ID
+	.get('/:id', (ctx) => {
+		return 'will show: ' + ctx.params.id;
+	})
+
+	// POST todo
+	.post(
+		'/',
+		(ctx) => {
+			return {
+				msg: 'will save this to the db',
+				body: ctx.body,
+			};
+		},
+		{
+			body: 'todo',
+		}
+	)
+
+	// PATCH todo
+	.patch(
+		'/:id',
+		(ctx) => {
+			return {
+				msg: 'will update: ' + ctx.params.id,
+				msg2: 'to this:',
+				body: ctx.body,
+			};
+		},
+		{
+			body: 'todo',
+		}
+	)
+
+	// DELETE a todo
+	.delete('/:id', (ctx) => {
+		return 'will delete: ' + ctx.params.id;
+	});
+
+// /todos/:id delete(DELETE)
+// /todos/:id update(PATCH)
