ec9890b698f87cc2ad4267b461fb19bd5fb794af
imkarimkarim <imkarimkarim@pm.me>
2023-10-31 07:09:39 +0330

    (ElysiaJS): mongoose CR test

--- ElysiaJS/todo-app-api/src/db/db.ts
+++ ElysiaJS/todo-app-api/src/db/db.ts
@@ -1,6 +1,38 @@
-import mongoose from 'mongoose';
+import mongoose, { Schema, model } from 'mongoose';
 
-const connection = process.env.MONGODB_URI ?? 'mongodb://localhost:27017';
+const connection = process.env.MONGODB_URI || 'mongodb://localhost:27017';
 
-mongoose.connect(connection).then(() => console.log('Connected to DataBase Successfully!'));
+mongoose.connect(connection);
+
+mongoose.connection
+	.on('open', () => console.log('mongoose is connected'))
+	.on('close', () => console.log('mongoose is disconnected'))
+	.on('error', (error) => console.log(error));
+
+// create a schema
+const userSchema = new Schema(
+	{
+		name: String,
+		age: Number,
+	},
+	{
+		timestamps: true,
+	}
+);
+
+// create a model
+const User = model('User', userSchema);
+
+const tmpUser = new User({
+	name: 'karim',
+	age: 23,
+});
+
+tmpUser.save().then((res) => {
+	console.log('res', res);
+	User.find({}).then((val) => {
+		console.log(val);
+	});
+});
+
 export default mongoose;
